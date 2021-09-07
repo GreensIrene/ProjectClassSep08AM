aa522a9800b5fa0b3b94b3541580b24b4bf9e0c8
GreensIrene <albeenairene28@gmail.com>
2021-09-07 15:17:21 +0530

    Username Functionality

--- src/org/pages/Login.java
+++ src/org/pages/Login.java
@@ -2,4 +2,11 @@ package org.pages;
 
 public class Login {
 
+	public static void userName() {
+		System.out.println("Username");
+	}
+
+	public static void main(String[] args) {
+		userName();
+	}
 }
