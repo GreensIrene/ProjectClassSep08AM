123b406eab62bcef10d0d280a996fec1aea7afbe
GreensIrene <albeenairene28@gmail.com>
2021-09-08 11:08:20 +0530

    LoadUrl

--- src/org/resources/LibGlobal.java
+++ src/org/resources/LibGlobal.java
@@ -2,4 +2,11 @@ package org.resources;
 
 public class LibGlobal {
 
+	public static void loadUrl() {
+		System.out.println("loadUrl");
+	}
+	
+	public static void main(String[] args) {
+		loadUrl();
+	}
 }
